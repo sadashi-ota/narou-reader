473010a038e714d738bd103bb577cf951a2a6523
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-10-10 01:56:56 +0900

    Add default parameter for novel search.

--- features/novel/novel_infra/src/main/kotlin/jp/sadashi/narou/reader/novel/infra/api/NovelSearchApiClient.kt
+++ features/novel/novel_infra/src/main/kotlin/jp/sadashi/narou/reader/novel/infra/api/NovelSearchApiClient.kt
@@ -5,11 +5,14 @@ import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+@Suppress("SpellCheckingInspection")
 interface NovelSearchApiClient {
 
     @GET("novelapi/api")
     fun searchNovel(
         @Query("out") out: String = "json",
-        @Query("word") word: String
+        @Query("word") word: String,
+        @Query("lim") limit: Int = 100,
+        @Query("order") order: String = "hyoka"
     ): Single<List<NovelSearchResult>>
 }
