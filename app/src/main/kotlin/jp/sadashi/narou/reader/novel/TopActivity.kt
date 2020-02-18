bc5e81383427703f1185474e249b40236b7a6abd
Sadashi Ota <sadashi.ota.dev@gmail.com>
2020-02-19 01:26:23 +0900

    Change top screen structure.

--- app/src/main/kotlin/jp/sadashi/narou/reader/novel/TopActivity.kt
+++ app/src/main/kotlin/jp/sadashi/narou/reader/novel/TopActivity.kt
@@ -2,12 +2,10 @@ package jp.sadashi.narou.reader.novel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import jp.sadashi.narou.reader.novel.ui.search.view.NovelSearchActivity
 
-class MainActivity: AppCompatActivity() {
+class TopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(NovelSearchActivity.callingIntent(this))
-        finish()
+        setContentView(R.layout.activity_top)
     }
 }
